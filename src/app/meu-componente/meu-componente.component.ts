8f8c3781a79b82a12d721a420b6c296c117b25ef
Akira Hanashiro <felipehanashiro@hotmail.com>
2018-07-23 07:12:44 -0300

    initial commit

--- src/app/meu-componente/meu-componente.component.ts
+++ src/app/meu-componente/meu-componente.component.ts
@@ -2,10 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-meu-componente',
-  //templateUrl: './meu-componente.component.html',
-  //styleUrls: ['./meu-componente.component.css'],
-  template: `<h1>Hello World!</h1>`,
-  styles: [`h1{ color: blue; }`]
+  templateUrl: './meu-componente.component.html',
+  styleUrls: ['./meu-componente.component.css'],
 })
 export class MeuComponenteComponent implements OnInit {
   
