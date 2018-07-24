f4362a126a7fc7bbc22468c23182759768327357
Akira Hanashiro <felipehanashiro@hotmail.com>
2018-07-24 18:12:29 -0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { ListComponent } from './views/list/list.component';
 import { ListItemComponent } from './components/list-item/list-item.component';
 import { RollOnScrollDirective } from './directives/roll-on-scroll.directive';
+import { MyNumberPipe } from './pipes/my-number.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListComponent,
     ListItemComponent,
-    RollOnScrollDirective
+    RollOnScrollDirective,
+    MyNumberPipe
   ],
   imports: [
     BrowserModule,
